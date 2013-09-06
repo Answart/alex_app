1d3386b4999fb39cd56a37c0495507b9bc5f02b0
Alexandra Swart <answart@sbcglobal.net>
2013-09-05 20:20:53 -0700

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ AlexApp::Application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
-  get "static_pages/navig"
+  get "static_pages/sitemap"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  subject { page }
+  # let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  # shared examples to eliminate the kind of duplication
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    # this = spec/support/utilities.rb
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+    it_should_behave_like "all static pages"
+
+    let(:heading)    { 'Alex App' }
+    let(:page_title) { '' }
+    it { should_not have_title('| Home') }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+    it_should_behave_like "all static pages"
+
+    let(:heading)    { 'Help' }
+    let(:page_title) { '' }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+    it_should_behave_like "all static pages"
+
+    let(:heading)    { 'About Us' }
+    let(:page_title) { '' }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    it_should_behave_like "all static pages"
+
+    let(:heading)    { 'Contact' }
+    let(:page_title) { '' }
+  end
+
+  describe "Site Map page" do
+  	before { visit sitemap_path }
+  	it_should_behave_like "all static pages"
+
+  	let(:heading)    { 'Site Map' }
+    let(:page_title) { '' }
+  end
+
+  # 
+#  it "should have the right links on the layout" do
+#    visit root_path
+#    # header
+#    click_link "sample app"
+#    expect(page).to have_content('Welcome to the Sample App')
+#    click_link "Home"
+#    click_link "Help"
+#    expect(page).to have_title(full_title('Help'))
+#    click_link "Sign up"
+#    expect(page).to have_title(full_title('Sign up'))
+    # footer
+#    click_link "About"
+#    expect(page).to have_title(full_title('About Us'))
+#    click_link "Contact"
+#    expect(page).to have_title(full_title('Contact'))
+#    click_link "Site Map"
+#    expect(page).to have_title(full_title('Site Map'))
+#  end
+
+end
